48975c6ee1cfc419aafcc6ac8b358289905699b1
sakklavut <sakklavut23395@gmail.com>
2023-03-13 17:16:29 +0700

    delete book

--- popular_books.php
+++ popular_books.php
@@ -58,24 +58,6 @@
               </div>     
           </div>
      </div>
-     <div class="d-none d-sm-block box_popular">
-          <img  class="cover_new" src="image/popular_books/popular_6.svg" alt="">
-          <div class="d-flex justify-content-end box_view">
-               <div class="me-2">
-                    <img src="icon/icon_eye_book.svg" alt="">
-               </div>
-               <div>
-                    <p class="view">25</p>
-               </div>
-          </div>
-          <div>
-               <div>
-                    <a href="http://" target="_blank">
-                         <p class="text_haeader_card_hit"title="Lorem Ipsum is simply dummy text">Lorem Ipsum is simply dummy text</p>
-                    </a>
-              </div>     
-          </div>
-     </div>
      <div class="box_popular">
           <img  class="cover_new" src="image/popular_books/popular_10.svg" alt="">
           <div class="d-flex justify-content-end box_view">
@@ -155,24 +137,6 @@
           <div class="d-flex justify-content-end box_view">
                <div class="me-2">
                      <img src="icon/icon_eye_book.svg" alt="">
-               </div>
-               <div>
-                    <p class="view">25</p>
-               </div>
-          </div>
-          <div>
-               <div>
-                    <a href="http://" target="_blank">
-                         <p class="text_haeader_card_hit"title="Lorem Ipsum is simply dummy text">Lorem Ipsum is simply dummy text</p>
-                    </a>
-              </div>     
-          </div>
-     </div>
-     <div class="d-none d-sm-block box_popular">
-          <img  class="cover_new" src="image/popular_books/popular_6.svg" alt="">
-          <div class="d-flex justify-content-end box_view">
-               <div class="me-2">
-                    <img src="icon/icon_eye_book.svg" alt="">
                </div>
                <div>
                     <p class="view">25</p>
