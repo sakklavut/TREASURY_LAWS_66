3fff35d4cf6d1e23cb8ff0df9b9bb518f9bf8bd9
sakklavut <sakklavut23395@gmail.com>
2023-03-17 17:28:10 +0700

    edit search

--- search_book.php
+++ search_book.php
@@ -1,4 +1,348 @@
-<div class="bg_main_search d-flex justify-content-center">
+<div class="container">
+     <div class="row">
+          <div class="col-xl-12 p-2 text-right">
+                <a href=""><img src="icon/circle-arrow-left-solid.svg" alt=""></a> 
+          </div>
+     </div>
+     <div class="row">
+          <div class="col-xl-12 text-center">
+                <h1 class="text_search">ค้นหาหนังสือ</h1>
+          </div>
+     </div>
+     <div class="row">
+          <div class="col-12 col-sm-3">
+                <select class="form-select form_title" aria-label="Default select example">
+                    <label for="" class="name_title">ชื่อเรื่อง</label>
+                         <option selected>ชื่อเรื่อง</option>
+                         <option value="1">เลขทะเบียนหนังสือ</option>
+                         <option value="2">ISBN</option>
+                         <option value="3">สำนักพิมพ์</option>
+                         <option value="3">ชื่อผู้แต่ง</option>
+                         <option value="3">หัวเรื่อง</option>
+                         <option value="3">ปีที่เพิมพ์</option>
+               </select>
+          </div>
+          <div class="col-12 col-sm-5">
+               <div class="row">
+                    <div class="col-12">
+                         <div class="input-group has-validation form_search">
+                              <span class="input-group-text">
+                                   <img src="icon/magnifying-glass-solid.svg" alt="">
+                              </span>
+                              <input type="text" class="form-control shadow-sm" id="inputsearch" placeholder="ค้นหาหนังสือที่น่าสนใจ" aria-describedby="inputGroupPrepend" srequired>
+                              <div class="invalid-feedback"></div>
+                         </div>
+                    </div>
+               </div>
+               <div class="row">
+                    <div class="col-4">
+                         <button type="submit" class="btn btn_tag mb-1">หนังมือใหม่</button>
+                    </div>
+                    <div class="col-4">
+                         <button type="submit" class="btn btn_tag mb-1">หนังสื่อยอดนิยม 10 อันดับ</button>
+                    </div>
+                    <div class="col-4">
+                          <button type="submit" class="btn btn_tag mb-1">ภาพกิจกรรม</button>
+                    </div>
+               </div>
+          </div>
+          <div class="col-12 col-sm-4">
+               <div class="row">
+                    <div class="col-sm-6">
+                          <a href="#"><button type="submit" class="btn btn_search box_btn" title="ค้นหา">ค้นหา</button> </a>
+                    </div>  
+                    <div class="col-sm-6">
+                          <a href="#"><button type="submit" class="btn btn_advanced box_btn" title="ค้นหาขั้นสูง">ค้นหาขั้นสูง</button> </a>
+                    </div>  
+               </div>
+          </div>
+     </div>
+</div>
+<div class="container ">
+     <div class="row">
+          <div class="col-12">
+               <div class="d-flex box_all">
+                    <div class="box_icon_search">
+                         <img src="icon/magnifying-glass-solid.svg" class="icon_search_results_w" title="ค้นหา" alt="">
+                    </div>
+                    <div class="box_search_results">
+                         <h4 class="search_results">ผลการค้นหา</h4>
+                    </div>
+               </div>
+          </div>
+     </div>
+    <hr class="hr_search">
+</div>
+<div class="container">
+     <div class="row">
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_1.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_2.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_3.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_4.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+     </div>
+     <div class="row">
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_1.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_2.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_3.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_4.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+     </div>
+     <div class="row">
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_1.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_2.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_3.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+          <div class="col-6 col-sm-3">
+               <div>
+                    <img class="cover_new" src="image/book/book_4.svg" alt="">
+                    <div class="d-flex justify-content-end box_view">
+                         <div class="me-2">
+                              <img src="icon/icon_eye_book.svg" alt="">
+                         </div>
+                         <div>
+                             <p class="view">25</p>
+                         </div>
+                    </div>
+                    <div>
+                        <div>
+                              <a href="http://" target="_blank" title="Lorem Ipsum is simply dummy text">
+                                   <p class="text_haeader_card">Lorem Ipsum is simply dummy text</p>
+                              </a>  
+                        </div> 
+                    </div>
+               </div>     
+          </div>
+     </div>
+</div>
+<div class="container d-flex justify-content-center">
+     <div class="row">
+          <nav aria-label="Page navigation example">
+               <ul class="pagination">
+                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+               </ul>
+          </nav>
+     </div>
+</div>
+
+
+
+
+
+
+
+
+<!-- <div class="bg_main_search d-flex justify-content-center">
      <div class="align-self-center">
           <div class="margin_book">
                <h1 class="text_search">ค้นหาหนังสือ</h1>
@@ -8,7 +352,6 @@
                     <form action="">
                          <div class="area_form">
                               <div class="d-flex">
-                                   
                                    <select class="form-select form_title" aria-label="Default select example">
                                    <label for="" class="name_title">ชื่อเรื่อง</label>
                                         <option selected>ชื่อเรื่อง</option>
@@ -248,4 +591,4 @@
                </ul>
           </nav>
      </div>
-</div>
+</div> -->
